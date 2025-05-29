cee011bbe120e6364c99d021eb54075fd6b3502f
jinyorjin <s223715707@deakin.edu.au>
2025-05-29 14:12:19 +1000

    weightlist errors

--- src/components/WeightList.js
+++ src/components/WeightList.js
@@ -35,7 +35,12 @@ function WeightList() {
           <div key={item.id} className="card mb-3">
             <div className="card-body d-flex justify-content-between align-items-center">
               <div>
-                <h5 className="card-title mb-1">{item.date}</h5>
+                <h5 className="card-title mb-1">
+                  {item.date?.seconds
+                    ? new Date(item.date.seconds * 1000).toLocaleDateString()
+                    : "No date"}
+                </h5>
+
                 <p className="card-text mb-0">{item.weight} kg</p>
               </div>
               <div>
